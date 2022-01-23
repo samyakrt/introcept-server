8a532937ddfdf074e17e90d9bbfdbeac7064fdd5
Samyak Tuladhar <tuladharsamyak@gmail.com>
2022-01-23 11:08:43 +0545

    register user

---
+++ app/Rules/ValidPhoneNumber.php
@@ -0,0 +1,42 @@
+<?php
+namespace App\Rules;
+use Illuminate\Contracts\Validation\Rule;
+
+class ValidPhoneNumber implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $first_three = substr($value,0,3);
+        if(in_array($first_three,config('services.sms.valid_numbers'))) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'The validation error message.';
+    }
+}
